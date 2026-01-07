e87f33df92cc85de05f6bcd7e69689a913a2cb7e
ABDUL-KABEER-1 <kabeerabdul714@gmail.com>
2026-01-08 04:01:51 +0500

    fix upload default export

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,1 +1,15 @@
-c
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "../config/cloudinary.js";
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "tomato-food",
+    allowed_formats: ["jpg", "png", "jpeg", "webp"],
+  },
+});
+
+const upload = multer({ storage });
+
+export default upload;   // 👈 THIS LINE IS MUST
